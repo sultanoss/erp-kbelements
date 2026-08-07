ab468d4b8096323a652ac811ba19d147c55d0575
Radwan <radwansultankassem@gmail.com>
2026-08-07 07:51:04 +0200

    chore: remove tmp migrate-customer-nums endpoint

---
+++ query_order.mjs
@@ -0,0 +1,13 @@
+﻿const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient({ datasources: { db: { url: '[SENSITIVE]' } } })
+async function main() {
+  const order = await prisma.order.findFirst({
+    where: { externalId: 'cbn4yr6cty' },
+    include: {
+      shipments: true,
+      items: true,
+    }
+  })
+  console.log(JSON.stringify(order, null, 2))
+}
+main().then(() => prisma.$disconnect()).catch(e => { console.error(e); prisma.$disconnect() })
